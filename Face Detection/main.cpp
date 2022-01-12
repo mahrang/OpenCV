800a5795742ae14f5551e80e73e7d3ce5602ad23
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2022-01-11 20:36:38 -0800

    Add files via upload

---
+++ Face Detection/main.cpp
@@ -0,0 +1,258 @@
+/*  This program detects faces in images/videos using Haar cascade classifier.
+ It outputs 2 windows:  1 showing faces detected with rectangles drawn around
+ the faces, the other with a skull superimposed onto the faces detected.
+ Inputs supplied were in this order:
+ --cascade=/Users/.../My_Code/haarcascade_frontalface_alt.xml
+ --scale=1.3
+ lena.jpg, faces.png, bday.jpg, or output.mp4
+ skull.jpg  */
+
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+static void help(const char** argv)
+{
+    cout << "\nThis program demonstrates the use of cv::CascadeClassifier class to detect objects (Face + eyes). You can use Haar or LBP features.\n"
+            "This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
+            "It's most known use is for faces.\n"
+            "Usage:\n"
+        <<  argv[0]
+        <<  "   [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
+            "   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
+            "   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
+            "   [--try-flip]\n"
+            "   [filename|camera_index]\n\n"
+            "example:\n"
+        <<  argv[0]
+        <<  " --cascade=\"data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"data/haarcascades/haarcascade_eye_tree_eyeglasses.xml\" --scale=1.3\n\n"
+            "During execution:\n\tHit any key to quit.\n"
+            "\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
+}
+
+void detectAndDraw( Mat& img, CascadeClassifier& cascade,
+                    CascadeClassifier& nestedCascade,
+                    double scale, bool tryflip, Mat& skull );
+
+string cascadeName;
+string nestedCascadeName;
+
+int main( int argc, const char** argv )
+{
+    VideoCapture capture;
+    Mat frame, image;
+    string inputName;
+    bool tryflip;
+    CascadeClassifier cascade, nestedCascade;
+    double scale;
+
+    cv::CommandLineParser parser(argc, argv,
+        "{help h||}"
+        "{cascade|data/haarcascades/haarcascade_frontalface_alt.xml|}"
+        "{nested-cascade|data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|}"
+        "{scale|1|}{try-flip||}{@filename||}"
+    );
+    if (parser.has("help"))
+    {
+        help(argv);
+        return 0;
+    }
+    cascadeName = parser.get<string>("cascade");
+    nestedCascadeName = parser.get<string>("nested-cascade");
+    scale = parser.get<double>("scale");
+    if (scale < 1)
+        scale = 1;
+    tryflip = parser.has("try-flip");
+    inputName = parser.get<string>("@filename");  // reads in 3rd file
+    Mat skull = imread(argv[4],1);
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 0;
+    }
+    if (!nestedCascade.load(samples::findFileOrKeep(nestedCascadeName)))
+        cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
+    if (!cascade.load(samples::findFile(cascadeName)))
+    {
+        cerr << "ERROR: Could not load classifier cascade" << endl;
+        help(argv);
+        return -1;
+    }
+    if( inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1) )
+    {
+        int camera = inputName.empty() ? 0 : inputName[0] - '0';
+        if(!capture.open(camera))
+        {
+            cout << "Capture from camera #" <<  camera << " didn't work" << endl;
+            return 1;
+        }
+    }
+    else if (!inputName.empty())  //if you have provided an image file
+    {
+        image = imread(samples::findFileOrKeep(inputName), IMREAD_COLOR);
+        if (image.empty())
+        {
+            if (!capture.open(samples::findFileOrKeep(inputName)))
+            {
+                cout << "Could not read " << inputName << endl;
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        image = imread(samples::findFile("lena.jpg"), IMREAD_COLOR);
+        if (image.empty())
+        {
+            cout << "Couldn't read lena.jpg" << endl;
+            return 1;
+        }
+    }
+
+    if( capture.isOpened() )    // if video file or webcam is provided
+    {
+        cout << "Video capturing has been started ..." << endl;
+
+        for(;;)
+        {
+            capture >> frame;
+            if( frame.empty() )
+                break;
+
+            Mat frame1 = frame.clone();
+            detectAndDraw( frame1, cascade, nestedCascade, scale, tryflip, skull );
+
+            char c = (char)waitKey(10);
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+        }
+    }
+    else  // otherwise use images
+    {
+        cout << "Detecting face(s) in " << inputName << endl;
+        if( !image.empty() )  // if image file is provided
+        {
+            detectAndDraw( image, cascade, nestedCascade, scale, tryflip, skull );
+            waitKey(0);
+        }
+        else if( !inputName.empty() )  // otherwise use text file w/list
+        {
+            /* assume it is a text file containing the
+            list of the image filenames to be processed - one per line */
+            FILE* f = fopen( inputName.c_str(), "rt" );
+            if( f )
+            {
+                char buf[1000+1];
+                while( fgets( buf, 1000, f ) )
+                {
+                    int len = (int)strlen(buf);
+                    while( len > 0 && isspace(buf[len-1]) )
+                        len--;
+                    buf[len] = '\0';
+                    cout << "file " << buf << endl;
+                    image = imread( buf, 1 );
+                    if( !image.empty() )
+                    {
+                        detectAndDraw( image, cascade, nestedCascade, scale, tryflip, skull );
+                        char c = (char)waitKey(0);
+                        if( c == 27 || c == 'q' || c == 'Q' )
+                            break;
+                    }
+                    else
+                    {
+                        cerr << "Aw snap, couldn't read image " << buf << endl;
+                    }
+                }
+                fclose(f);
+            }
+        }
+    }
+
+    return 0;
+}
+
+void detectAndDraw( Mat& img, CascadeClassifier& cascade,
+                    CascadeClassifier& nestedCascade,
+                    double scale, bool tryflip,  Mat& skull)
+{
+    double t = 0;
+    vector<Rect> faces, faces2;
+    const static Scalar colors[] =
+    {
+        Scalar(255,0,0),
+        Scalar(255,128,0),
+        Scalar(255,255,0),
+        Scalar(0,255,0),
+        Scalar(0,128,255),
+        Scalar(0,255,255),
+        Scalar(0,0,255),
+        Scalar(255,0,255)
+    };
+    Mat gray, smallImg;
+
+    cvtColor( img, gray, COLOR_BGR2GRAY );
+    double fx = 1 / scale;
+    resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT );
+    equalizeHist( smallImg, smallImg );
+
+    t = (double)getTickCount();
+    cascade.detectMultiScale( smallImg, faces,
+        1.1, 2, 0
+        //|CASCADE_FIND_BIGGEST_OBJECT
+        //|CASCADE_DO_ROUGH_SEARCH
+        |CASCADE_SCALE_IMAGE,
+        Size(30, 30) );
+    if( tryflip )
+    {
+        flip(smallImg, smallImg, 1);
+        cascade.detectMultiScale( smallImg, faces2,
+                                 1.1, 2, 0
+                                 //|CASCADE_FIND_BIGGEST_OBJECT
+                                 //|CASCADE_DO_ROUGH_SEARCH
+                                 |CASCADE_SCALE_IMAGE,
+                                 Size(30, 30) );
+        for( vector<Rect>::const_iterator r = faces2.begin(); r != faces2.end(); ++r )
+        {
+            faces.push_back(Rect(smallImg.cols - r->x - r->width, r->y, r->width, r->height));
+        }
+    }
+    t = (double)getTickCount() - t;
+    printf( "detection time = %g ms\n", t*1000/getTickFrequency());
+    Mat newFrame=img.clone();
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Rect r = faces[i];
+        Mat smallImgROI;
+        vector<Rect> nestedObjects;
+        Point center;
+        Scalar color = colors[i%8];
+
+        resize(skull, skull, Size((r.width-1)*scale, (r.height-1)*scale), 0, 0, INTER_CUBIC);
+        Rect roi = Rect(r.x*scale, r.y*scale, (r.width-1)*scale,
+                        (r.height-1)*scale);
+        
+        Mat dstROI = newFrame(roi);  // dstROI is an image of just the face
+        skull.copyTo(dstROI);  //part a, skull is copied to dstROI
+        // for part b, comment out the previous line and uncomment the next line
+        //addWeighted( skull, 0.8, dstROI, 0.2, 0.0, dstROI );
+        //addWeighted equation is g = f1 x alpha + f2 x beta + gamma
+        // where f1 & f2 are image1 & image2 respectively
+        // beta = 1 - alpha
+        // in our case, the equation is
+        // dstROI = skull x alpha + dstROI x beta + gamma
+        // in the above equation, alpha = 0.8, beta = 0.2, & gamma = 0
+        imshow( "show skulls", newFrame );
+
+        // draw rectangle around face on original image
+            rectangle( img, Point(cvRound(r.x*scale), cvRound(r.y*scale)),
+                       Point(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale)),
+                       color, 3, 8, 0);
+    }
+    imshow( "detect faces", img );
+  }
